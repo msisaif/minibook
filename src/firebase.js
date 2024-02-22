4a87880ee180392251c09edf4d7364216c0e2b4a
MD SAIFUL ISLAM <61042251+msisaif@users.noreply.github.com>
2024-02-22 19:32:18 +0000

    analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,12 +22,14 @@ const firebaseConfig = {
   storageBucket: "minibook-313.appspot.com",
   messagingSenderId: "75680827767",
   appId: "1:75680827767:web:d9d0008238c4f254683972",
-  // measurementId: "G-F7HCCGNMEW",
+  measurementId: "G-F7HCCGNMEW",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
+
+logEvent(analytics, 'notification_received');
 
 const auth = getAuth(app);
 
